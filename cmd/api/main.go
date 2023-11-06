a15f5ec23076863f62afcc65de6636a219a63e8d
rolando <jakob.b.work2@gmail.com>
2023-11-06 20:14:41 +0100

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+  Domain string
+}
+
+func main() {
+  // set application config
+  var app application
+
+  // read from command line
+
+  // connect to the database
+
+  app.Domain = "example.com"
+  log.Println("Starting the server on port", port)
+
+  http.HandleFunc("/", Hello)
+
+  // start a web server
+  err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+  if err != nil {
+    log.Fatal("Error starting http server: ", err)
+  }
+}
